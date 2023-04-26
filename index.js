85f70a572b943dd3d653800c2d61d4a3d0d40ce0
Tim Heuett <tim.heuett@gmail.com>
2023-04-26 11:36:30 -0700

    dumping some code out of this old glitch bot that I never finished and am now repurposing

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const { Client, Intents } = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-
-const discordToken = process.env.DISCORD_APP_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once("ready", () => {
-  console.log("Ready!");
+    console.log("Bot is ready");
+});
+
+client.on("message", (message) => {
+    console.log("message received", message);
 });
 
 client.login(process.env.DISCORD_API_TOKEN);
