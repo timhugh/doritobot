5b16e309c1db180e5cb332f66d71a333acb25f8f
Tim Heuett <tim.heuett@gmail.com>
2023-04-26 16:33:59 -0700

    clean up examples

--- examples/bisect/stop_server.sh
+++ examples/bisect/stop_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 source "$(git rev-parse --show-toplevel)/.env"
-curl -s -XPOST \
-    "https://games.bisecthosting.com/api/client/servers/${BISECT_SERVER_ID}/power" \
+curl -s \
+    -XPOST "https://games.bisecthosting.com/api/client/servers/${BISECT_SERVER_ID}/power" \
     -H "Authorization: Bearer ${BISECT_API_TOKEN}" \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
