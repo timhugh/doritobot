8482736849695b3df6f7c7faecadc61c4e337e70
Glitch (glitch-hello-node) <none>
2023-04-26 08:00:20 +0000

    🌦🏯 Checkpoint ./README.md:5198005/12 ./index.js:5198005/1400

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const { Client, Intents } = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
 const discordToken = process.env.DISCORD_APP_TOKEN;
-const clientId = process.env.DISCORD_CLIENT_ID;
-const guildId = process.env.DISCORD_GUILD_ID;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const commands = [
-  
-]
-
-client.once('ready', () => {
-  console.log('Ready!');
+client.once("ready", () => {
+  console.log("Ready!");
 });
 
-client.login(discordToken);
+client.login(process.env.DISCORD_API_TOKEN);
